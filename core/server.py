2358f787a33242376bcb79c4722856ec6ca25936
tarunraghav226 <tarun.raghav226@gmail.com>
2021-10-05 17:22:51 +0530

    Added route for teacher

--- core/server.py
+++ core/server.py
@@ -1,7 +1,7 @@
 from flask import jsonify
 from marshmallow.exceptions import ValidationError
 from core import app
-from core.apis.assignments import student_assignments_resources
+from core.apis.assignments import student_assignments_resources, teacher_assignments_resources
 from core.libs import helpers
 from core.libs.exceptions import FyleError
 from werkzeug.exceptions import HTTPException
@@ -9,6 +9,7 @@ from werkzeug.exceptions import HTTPException
 from sqlalchemy.exc import IntegrityError
 
 app.register_blueprint(student_assignments_resources, url_prefix='/student')
+app.register_blueprint(teacher_assignments_resources, url_prefix='/teacher')
 
 
 @app.route('/')
@@ -24,8 +25,9 @@ def ready():
 @app.errorhandler(Exception)
 def handle_error(err):
     if isinstance(err, FyleError):
+        res = err.to_dict()
         return jsonify(
-            error=err.__class__.__name__, message=err.message
+            error=err.__class__.__name__, message=res["message"]
         ), err.status_code
     elif isinstance(err, ValidationError):
         return jsonify(
@@ -39,5 +41,9 @@ def handle_error(err):
         return jsonify(
             error=err.__class__.__name__, message=str(err)
         ), err.code
+    elif isinstance(err, ValueError):
+        return jsonify(
+            error=err.__class__.__name__, message=str(err)
+            ), 400
 
     raise err
